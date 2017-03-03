d1eaca7f02fbad3a9f3896026f665fc7e0709dfc
Thomas Lundquist <github@bisonlab.no>
2017-03-03 09:54:27 +0100

    In case of no retriever, handle it. And some more comments about the thingie.

--- Service/ExternalRetriever.php
+++ Service/ExternalRetriever.php
@@ -9,6 +9,20 @@ namespace BisonLab\CommonBundle\Service;
  * You will need a retriever service per external system. (Which of course can
  * point at the same retriever file.)
  * 
+ * I guess it should implement an interface, but with only one single 
+ * function? I'm lazy.
+ *
+ * There is also another annoying point here. And that is when there are more
+ * than one object related to the context.
+ *
+ * I'll chicken out and let the two ends of this decide how they like it.
+ * (They should know what they are dealing with and therefor know how to handle
+ * one or more returned objects or arrays.)
+ *
+ * The retriever end also has to remember security. It should ponder a bit
+ * about who is asking.
+ *
+ * And I should ponder about how they can find out.
  */
 
 class ExternalRetriever
@@ -24,6 +38,10 @@ class ExternalRetriever
     {
         $rname = strtolower($context->getSystem() . "_retriever");
         $retriever = $this->container->get($rname);
-        return $retriever->getExternalDataFromContext($context);
+        if (is_object($retriever)
+            && method_exists($retriever, 'getExternalDataFromContext'))
+                return $retriever->getExternalDataFromContext($context);
+        else
+            return null;
     }
 }
