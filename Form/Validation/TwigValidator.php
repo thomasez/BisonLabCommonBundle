7f23b1dba0f35c6c50d025ab18aadc512d6018f7
Inventory <inventory@nteb.no>
2017-12-07 08:52:25 +0100

    Attempting to make the twig validator a service.

--- Form/Validation/TwigValidator.php
+++ Form/Validation/TwigValidator.php
@@ -11,7 +11,30 @@ use Symfony\Component\Validator\ConstraintValidator;
 
 class TwigValidator extends ConstraintValidator
 {
+    private $twig;
+
+    public function __construct($twig)
+    {
+error_log("Ja");
+        $this->twig = $twig;
+    }
+    
     public function validate($value, Constraint $constraint)
+    {
+error_log("Funk");
+        $bin2hex_filter = new \Twig_SimpleFilter('bin2hex', 'bin2hex');
+        $this->twig->addFilter($bin2hex_filter);
+        $this->twig->addExtension(new \Twig_Extension_StringLoader());
+        
+        try {
+            $nodeTree = $this->twig->parse($this->twig->tokenize($value));
+        } catch (\Twig_Error $e) {
+            $this->context->buildViolation($e->getMessage())
+                ->addViolation();
+        }
+    }
+
+    public function oldis($value, Constraint $constraint)
     {
         $loader = new \Twig_Loader_Array();
         $twig = new \Twig_Environment($loader, array(
