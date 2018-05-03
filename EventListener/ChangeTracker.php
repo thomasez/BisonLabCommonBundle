15bb673b909d379585d5b27e20e7f648b5b9e00a
Dabaru Application User <dabaru@dabaru-dev.nteb.no>
2018-05-03 11:45:59 +0200

    Better handle automatic lowercasing of attributes here than in the dirrefent applications.

---
+++ EventListener/ChangeTracker.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace BisonLab\CommonBundle\EventListener;
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+
+/*
+ * Does as little as possible.
+ */
+
+class ChangeTracker 
+{
+    public function preUpdate(PreUpdateEventArgs $eventArgs)
+    {
+        if ($eventArgs->hasChangedField('attributes_json')) {
+            if ($json = $eventArgs->getNewValue('attributes_json')) {
+                $la = $this->array_change_key_case_recursive(json_decode($json, true), MB_CASE_LOWER);
+                $json = json_encode($la);
+                $eventArgs->setNewValue('attributes_json', $json);
+            }
+        }
+    }
+}
