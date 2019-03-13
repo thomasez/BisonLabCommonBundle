f3ce55ffc94d565955061874f9d18d9f9ff6bd89
Dabaru Application User <dabaru@dabaru-dev.nteb.no>
2019-03-13 16:02:53 +0100

    Adding findByContext

--- Entity/ContextRepositoryTrait.php
+++ Entity/ContextRepositoryTrait.php
@@ -6,27 +6,23 @@ use Doctrine\ORM\Mapping as ORM;
 
 trait ContextRepositoryTrait
 {
-    /* 
-     * I'll make this one private since this is called from the Repo traiting
-     * this. 
-     */
-    public function _getOneByContext($owner_class, $system, $object_name, $external_id, $hydrationMode = \Doctrine\ORM\Query::HYDRATE_OBJECT)
+    public function getOneByContext($system, $object_name, $external_id)
     {
-        // This is so annoyng! I Just did not get subselects working, at all.
-        $qb = $this->_em->createQueryBuilder();
-        $qb->select('oc')
-              ->from($owner_class, 'oc')
-              ->where('oc.system = :system')
-              ->andWhere('oc.object_name = :object_name')
-              ->andWhere('oc.external_id = :external_id')
+        return current($this->findByContext($system, $object_name, $external_id));
+    }
+
+    public function findByContext($system, $object_name, $external_id)
+    {
+        $qb2 = $this->_em->createQueryBuilder();
+        $qb2->select('e')
+              ->from($this->_entityName, 'e')
+              ->innerJoin('e.contexts', 'ec')
+              ->where('ec.system = :system')
+              ->andWhere('ec.object_name = :object_name')
+              ->andWhere('ec.external_id = :external_id')
               ->setParameter("system", $system)
               ->setParameter("object_name", $object_name)
-              ->setParameter("external_id", $external_id)
-              ->setMaxResults(1);
-
-        $context = $qb->getQuery()->getResult();
-
-        if (empty($context)) { return null; }
-        return current($context)->getOwner();
+              ->setParameter("external_id", $external_id);
+        return $qb2->getQuery()->getResult();
     }
 }
