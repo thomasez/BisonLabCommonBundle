60c084e77f533dd89929472b1ec063f0842f4f0b
Thomas Lundquist <thomasez@bisonlab.no>
2020-01-08 17:28:54 +0100

    Seems to be giving the menu the classes and attributes mentioned in bootstrap 4 docs.

--- Menu/StylingTrait.php
+++ Menu/StylingTrait.php
@@ -6,6 +6,13 @@ namespace BisonLab\CommonBundle\Menu;
  * This should somehow be able to get the preferred menu style and use it.
  *
  * For now it's my preferred style, bootstrap tabs.
+ *
+ * You need to add this to config.yml / .env
+    knp_menu:
+        renderer.twig.options:
+            currentClass: active
+            ancestorClass: active
+            allow_safe_labels: true
  */
 trait StylingTrait
 {
@@ -14,11 +21,14 @@ trait StylingTrait
         $menu->setChildrenAttribute('class', 'nav nav-tabs');
         foreach ($menu->getChildren() as $child) {
             // Why is this not working?
+            // Solution:
             if ($child->isCurrent()) {
                 $child->setAttribute('class', 'active');
             }
             if (count($child->getChildren()) > 0) {
                 $child
+                    ->setLabel($child->getLabel() . '<b class="caret"></b>')
+                    ->setExtra('safe_label', true)
                     ->setUri('#')
                     ->setLinkAttribute('class', 'nav-link dropdown-toggle')
                     ->setLinkAttribute('data-toggle', 'dropdown')
