75cb28961584c62e9da937d5aaf03e72bfd54938
Thomas Lundquist <thomasez@bisonlab.no>
2021-10-30 14:11:46 +0200

    Part of splitting out contextbundle.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -13,7 +13,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritDoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('bisonlab_common');
         return $treeBuilder;
