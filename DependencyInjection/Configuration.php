8e8486f5ab9ea13c53021eaade5c73b6dd4d49bd
Thomas Lundquist <thomasez@bisonlab.no>
2024-12-23 16:52:43 +0100

    Revert "No need for these any more."
    This reverts commit 64fc7d5ab71cdb390307240603d038312c2ea3e9.

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace BisonLab\CommonBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Not much at all.
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('bisonlab_common');
+        return $treeBuilder;
+    }
+}
